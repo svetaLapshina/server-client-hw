8cc272465fda1d0282f52701fe769342284f213d
sveta <sveta.lapshina@gmail.com>
2022-04-16 22:52:55 +0200

    First commit

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket("localhost", 8080);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Hogwarts");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
